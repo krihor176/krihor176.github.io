13fb27a4cddee0784c738e13833f80e59f54c4e2
krihor176 <krihor01@easv365.dk>
2024-01-24 09:50:18 +0100

    new stuff

--- script.js
+++ script.js
@@ -211,5 +211,12 @@
   
     return true;
 
-    
+    function myFunction() {
+      var x = document.getElementById("myLinks");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
   }
